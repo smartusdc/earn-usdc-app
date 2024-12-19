ea3870e2c3fbb5ac7975bafe073ee03dc8b44582
smartusdc <jakepaul900009@gmail.com>
2024-12-20 06:23:54 +0900

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,24 @@
 // src/config.ts
-export const CONFIG = {
+type ContractConfig = {
+  CHAIN_ID: number;
+  APR: number;
+  MIN_DEPOSIT_USDC: number;
+  MAX_DEPOSIT_USDC: number;
+  DECIMALS: number;
+  ADMIN_WALLET: string | undefined;
+  CONTRACT_ADDRESS: `0x${string}` | undefined;
+  CONTRACT_ABI: any[];
+};
+
+export const CONFIG: ContractConfig = {
   CHAIN_ID: 8453,
   APR: 24,
   MIN_DEPOSIT_USDC: 0.1,
   MAX_DEPOSIT_USDC: 1000000000000,
   DECIMALS: 6,
   ADMIN_WALLET: process.env.NEXT_PUBLIC_ADMIN_WALLET,
-  CONTRACT_ADDRESS: process.env.NEXT_PUBLIC_CONTRACT_ADDRESS,
+  CONTRACT_ADDRESS: process.env.NEXT_PUBLIC_CONTRACT_ADDRESS as `0x${string}`,
   CONTRACT_ABI: [
-    // ABIÇÇ±Ç±Ç…íºê⁄ãLèq
+    // ABI contents
   ]
 } as const;
